b6feb5a5df004d81c913b618d9e4bd274e1c3fd7
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-09-08 11:39:10 -0300

    feat: models products

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class CategorysChoices(models.TextChoices):
+    ROUPAS     = "Roupas"
+    ACESSORIOS = "Acessórios"
+    JOIAS      = "Jóias"
+    RELOGIOS   = "Relógios"
+    BOLSAS     = "Bolsas"
+    CALÇADOS   = "Calçados"
+    DEFAULT    = "Geral"
+    
+
+class Product(models.Model):
+    id          = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image       = models.ImageField(upload_to=None, height_field=400, width_field=600, max_length=250)
+    name        = models.CharField(max_length=257)
+    description = models.TextField()
+    price       = models.DecimalField(max_digits=10, decimal_places=2) 
+    category    = models.CharField(max_length=20, choices=CategorysChoices.choices, default=CategorysChoices.DEFAULT)
+    is_active   = models.BooleanField(default=True)
+    is_new      = models.BooleanField()
+    is_selled   = models.BooleanField(default=False)
+    
+    seller = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name="products" )
