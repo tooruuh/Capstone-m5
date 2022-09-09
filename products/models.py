b958f088de0f038cd805c8c1bf1c1098110764f8
Wellington José <paivawellington23@gmail.com>
2022-09-09 13:51:51 -0300

    fix: fixed login user

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class CategorysChoices(models.TextChoices):
 
 class Product(models.Model):
     id          = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image       = models.ImageField(upload_to=None, height_field=400, width_field=600, max_length=250)
+    # image       = models.ImageField(upload_to=None, height_field=400, width_field=600, max_length=250)
     name        = models.CharField(max_length=257)
     description = models.TextField()
     price       = models.DecimalField(max_digits=10, decimal_places=2) 
--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+import email
 from rest_framework import serializers
 
 from .models import User
@@ -5,11 +6,17 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id","username","email","password","plan","wallet","updated_at","created_at"]
+        fields = ["id","name","email","password","plan","wallet","updated_at","created_at"]
         extra_kwargs = {
-            "password": {"write_only": True}
+            "password": {"write_only": True},
+            "wallet": {"read_only": True}
         }
     
     def create(self, validated_data: dict):
+        validated_data["username"] = validated_data["email"]
         user = User.objects.create_user(**validated_data)
         return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True)
