cb114ee8cc6e743731adecfd3f4a7418ad9fcc77
Felipe Barcellos <felipebarcellos11@gmail.com>
2022-09-09 10:08:50 -0300

    FEAT: serializer products, urls create products, custom_permission products, view post and get products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from .custom_permissions import IsSellerIsExact, IsSellerOrReadOnly
+
+from .serializer import ProductSerializer
+from .models import Product
+
+
+class ProductGenericView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsSellerOrReadOnly]
+    
+    queryset = Product.objects.all()
+    
+    def get_serializer_class(self):
+        return ProductSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(seller=self.request.user)
+        
+        
+class ProductDetailView(generics.RetrieveUpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsSellerIsExact]
+    
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    lookup_url_kwarg = "product_id"
